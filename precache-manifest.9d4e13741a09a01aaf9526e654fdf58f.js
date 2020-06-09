bd405339e94ab3b893710c00a7697ec6364d7f25
usamabinzubair <usamazubair@hotmail.co.uk>
2020-06-09 23:33:50 +0100

    Updates

---
+++ precache-manifest.9d4e13741a09a01aaf9526e654fdf58f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfefe13d018e924458ae3e20a5fde5cc",
+    "url": "/redditclone/index.html"
+  },
+  {
+    "revision": "7ea2001b2675bd19b47f",
+    "url": "/redditclone/static/css/main.8ba9324a.chunk.css"
+  },
+  {
+    "revision": "5acd1e8d008af4383b71",
+    "url": "/redditclone/static/js/2.08d448a5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/redditclone/static/js/2.08d448a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea2001b2675bd19b47f",
+    "url": "/redditclone/static/js/main.00f8135a.chunk.js"
+  },
+  {
+    "revision": "82a81f9d90f13b43a609",
+    "url": "/redditclone/static/js/runtime-main.5cac8e26.js"
+  },
+  {
+    "revision": "45c7a03743d29830856449725d146559",
+    "url": "/redditclone/static/media/cat.45c7a037.jpg"
+  },
+  {
+    "revision": "5ed17fa2834b9181138f0aaa41996f78",
+    "url": "/redditclone/static/media/cmv.5ed17fa2.png"
+  },
+  {
+    "revision": "330f68b9fc57866a6cd2e13d9aab4781",
+    "url": "/redditclone/static/media/corona.330f68b9.png"
+  },
+  {
+    "revision": "bdf4ab0f58e21930385e01a434d86094",
+    "url": "/redditclone/static/media/man.bdf4ab0f.png"
+  },
+  {
+    "revision": "4245c6e07f801bd3a57a1d0e03318098",
+    "url": "/redditclone/static/media/redditlogo.4245c6e0.png"
+  },
+  {
+    "revision": "287513a5b60641c0f4cb9ffed541aec6",
+    "url": "/redditclone/static/media/reddittext.287513a5.png"
+  },
+  {
+    "revision": "5da02823498ae0b1fe1070dfb5a65e0c",
+    "url": "/redditclone/static/media/salami.5da02823.png"
+  }
+]);
